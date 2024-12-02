09b66fd788e657fde843c979382810c2ec066545
Nikolai Balck <116642207+B4lck@users.noreply.github.com>
2024-12-02 09:32:10 +0100

    module og noget andet noget

---
+++ System/src/model/Date.java
@@ -0,0 +1,51 @@
+package model;
+
+public class Date {
+    private int day;
+    private int month;
+    private int year;
+
+    public Date(int day, int month, int year) {
+        set(day, month, year);
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getDays() {
+        return day * 86400 + month * 3600 + year * 31556926;
+    }
+
+    public void set(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%02d/%02d/%02d", day, month, year);
+    }
+
+    public Date copy() {
+        return new Date(day, month, year);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || !(obj.getClass() == getClass())) {
+            return false;
+        }
+        Date d = (Date) obj;
+        return day == d.day && month == d.month && year == d.year;
+    }
+}
