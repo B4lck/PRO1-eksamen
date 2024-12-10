166721783a6a927ab2a3d383e92f8ec5a372e1ce
Nikolai Balck <116642207+B4lck@users.noreply.github.com>
2024-12-10 12:13:03 +0100

    burger menu navbar på forside

--- Hjemmeside/script.js
+++ Hjemmeside/script.js
@@ -1,1 +1,16 @@
 console.log("Hello, World!");
+
+// Burger menu
+const burgerIcon = document.getElementById("burger-icon");
+const navbar = document.getElementById("navbar");
+
+burgerIcon.addEventListener("touchend", function() {
+    setTimeout(() => {
+        navbar.classList.toggle("openNavbar");
+    }, 1);
+})
+
+document.body.addEventListener("touchend", function() {
+    navbar.classList.remove("openNavbar");
+})
+
